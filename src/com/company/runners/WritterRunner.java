17920fed504a73e3709b5b7ee251b86abf63568d
nataliesmithmsm <natalie.smith@moneysupermarket.com>
2017-11-28 13:35:46 +0000

    Back to normal

--- src/com/company/runners/WritterRunner.java
+++ src/com/company/runners/WritterRunner.java
@@ -4,8 +4,8 @@ import com.company.filehelpers.FileReaderWriterInitializer;
 
 public class WritterRunner {
 
-//    public static void main(String[] args) {
-//        FileReaderWriterInitializer fileReaderWriterInitializer = new FileReaderWriterInitializer();
-//        fileReaderWriterInitializer.readCsvFileAndWriteMongo("ReadMe copy.csv");
-//    }
+    public static void main(String[] args) {
+        FileReaderWriterInitializer fileReaderWriterInitializer = new FileReaderWriterInitializer();
+        fileReaderWriterInitializer.readCsvFileAndWriteMongo("ReadMe copy.csv");
+    }
 }
